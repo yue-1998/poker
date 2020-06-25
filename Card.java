6d48f460ea3d9d1f8866ec2f92a39c513d679fb4
yue-1998 <1012336581@qq.com>
2020-06-25 13:47:53 +0800

    finish basic functions

--- Card.java
+++ Card.java
@@ -32,12 +32,64 @@ public class Card {
 		}
 	}
 	// 输出函数（打印信息）
-	void dealOutput() {
-		
+	String dealOutput(int black,int white,int blacknum,int whitenum) {
+		String result = "";
+		if(black > white) {
+			result = "Black wins";
+		}else if(black < white) {
+			result = "White wins";
+		}else if(black == white) {
+			if(blacknum > whitenum) {
+				result = "Black wins";
+			}else if(blacknum < whitenum) {
+				result = "White wins";
+			}else if(blacknum == whitenum) {
+				result = "Tie";
+			}
+		}
+		return result;
 	}
 	// 比较函数
-	void Compare() {
-		
+	int Compare(int num[],String decor[]) {
+		int result = 0;
+		result = dealType(num,decor);
+		return result;
+	}
+	int dealType(int num[],String decor[]) {
+		int result = 0;
+		if(dealDuiZi(num)) {
+			result = 1;
+		}else if(dealLiangDui(num)) {
+			result = 2;
+		}else if(dealSanTiao(num)) {
+			result = 3;
+		}else if(dealShunZi(num)) {
+			result = 4;
+		}else if(dealTongHua(num,decor)) {
+			result = 5;
+		}else if(dealTongHuaShun(num,decor)) {
+			result = 6;
+		}else {
+			result = 0;
+			num[5] = num[4];
+		}
+		return result;
+	}
+	void dealBubble(int num[],String decor[]) {
+		int i,j,tempnum;
+		String tempdecor;
+		for(i = 0;i < 5;i++) {
+			for(j = 0;j < 4-i;j++) {
+				if(num[j] > num[j+1]) {
+					tempnum = num[j+1];
+					num[j+1] = num[j];
+					num[j] = tempnum;
+					tempdecor = decor[j+1];
+					decor[j+1] = decor[j];
+					decor[j] = tempdecor;
+				}
+			}
+		}
 	}
 	// 判断是否为同花顺
 	boolean dealTongHuaShun(int num[],String decor[]) {
@@ -47,6 +99,15 @@ public class Card {
 				result = true;
 				num[5] = num[4];
 			}
+		}
+		return result;
+	}
+	// 判断是否为同花
+	boolean dealTongHua(int num[],String decor[]) {
+		boolean result = false;
+		if(decor[0].equals(decor[1]) && decor[1].equals(decor[2]) && decor[2].equals(decor[3]) && decor[3].equals(decor[4])) {
+			result = true;
+			num[5] = num[4];
 		}
 		return result;
 	}
@@ -87,9 +148,23 @@ public class Card {
 		return result;
 	}
 	// 判断是否为对子
-//	boolean dealDuiZi(int num[]) {
-//		
-//	}
+	boolean dealDuiZi(int num[]) {
+		boolean result = false;
+		if(num[0] == num[1]) {
+			result = true;
+			num[5] = num[1];
+		}else if(num[1] == num[2]) {
+			result = true;
+			num[5] = num[2];
+		}else if(num[2] == num[3]) {
+			result = true;
+			num[5] = num[3];
+		}else if(num[3] == num[4]) {
+			result = true;
+			num[5] = num[4];
+		}
+		return result;
+	}
 	// 调用
 	String GetValue(String black[], String white[]) {
 		String result = null;
@@ -97,16 +172,18 @@ public class Card {
 		int whitenum[] = new int[] {0,0,0,0,0,0};
 		String blackdecor[] = new String[] {"","","","",""};
 		String whitedecor[] = new String[] {"","","","",""};
-		boolean blackT = false;
-		boolean whiteT= false;
+		int br = 0;
+		int wr = 0;
 		dealInput(black,blacknum,blackdecor);
 		dealInput(white,whitenum,whitedecor);
-		blackT = dealTongHuaShun(blacknum,blackdecor);
-		blackT = dealShunZi(blacknum);
-		blackT = dealSanTiao(blacknum);
-		blackT = dealLiangDui(blacknum);
-		System.out.println(blackT);
-		System.out.println(blacknum[5]);
+		
+		dealBubble(blacknum,blackdecor);
+		dealBubble(whitenum,whitedecor);
+		
+		br = Compare(blacknum,blackdecor);
+		wr = Compare(whitenum,whitedecor);
+		
+		result = dealOutput(br,wr,blacknum[5],whitenum[5]);
 		return result;
 	}
 }
--- TestCard.java
+++ TestCard.java
@@ -5,11 +5,45 @@ public class TestCard {
 	@Test
 	public void TestCase1() {
 		Card card = new Card();
-		// 散牌 vs 散牌
-//		String black[] = {"2H","3D","5S","9C","KD"};
-		String black[] = {"2H","6H","6H","7H","7H"};
+		String black[] = {"2H","3D","5S","9C","KD"};
 		String white[] = {"2C","3H","4S","8C","AH"};
 		String expectedResult = card.GetValue(black,white);
 		System.out.println(expectedResult == "White wins");
+	}
+	
+	@Test
+	public void TestCase2() {
+		Card card = new Card();
+		String black[] = {"2H","4S","4C","2D","4H"};
+		String white[] = {"2S","8S","AS","QS","3S"};
+		String expectedResult = card.GetValue(black,white);
+		System.out.println(expectedResult == "White wins");
+	}
+	
+	@Test
+	public void TestCase3() {
+		Card card = new Card();
+		String black[] = {"2H","3H","5H","9H","KH"};
+		String white[] = {"2C","3H","4S","5C","6H"};
+		String expectedResult = card.GetValue(black,white);
+		System.out.println(expectedResult == "Black wins");
+	}
+	
+	@Test
+	public void TestCase4() {
+		Card card = new Card();
+		String black[] = {"2H","3D","5S","9C","KD"};
+		String white[] = {"2D","3H","5C","9S","KH"};
+		String expectedResult = card.GetValue(black,white);
+		System.out.println(expectedResult == "Tie");
+	}
+	
+	@Test
+	public void TestCase5() {
+		Card card = new Card();
+		String black[] = {"6H","7H","8H","9H","10H"};
+		String white[] = {"6D","7D","8D","9D","10D"};
+		String expectedResult = card.GetValue(black,white);
+		System.out.println(expectedResult == "Tie");
 	}
 }
